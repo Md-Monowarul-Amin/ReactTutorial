0227ee9b3fdc75cf4b97df8284159dd0547e8367
Md-Monowarul-Amin <aminsaraj2015@gmail.com>
2024-03-03 09:10:27 +0600

    ClickNumberAdded

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// function handleClick({value}){
+//   alert({value});
+// }
+
+function Square({num}){
+  const [value, setValue] = useState("X");
+  function handleClick(){
+    setValue(num);
+  }
+  return <button className='square'
+                  onClick={handleClick}
+                  >
+                    {value} 
+                  </button>
+}
+
+function Board(){
+  return(
+    <>
+      <div className='board-row'>
+        <Square num="1" />
+        <Square num="2" />
+        <Square num="3" />
+      </div>
+      <div className='board-row'>
+        <Square num="4" />
+        <Square num="5" />
+        <Square num="6" />
+      </div>
+      <div className='board-row'>
+        <Square num="7" />
+        <Square num="8" />
+        <Square num="9" />
+      </div>
+    </>
+  )
+}
+
+export default Board;
